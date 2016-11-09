a4d766b5b2a0fc7043a0fec2f3544bf84b843812
Barbara Boeters <BarbaraBoeters@MacBook-Pro-van-Barbara.local>
2016-11-09 22:38:43 +0100

    Initial Commit

---
+++ barbaraboeters-pset2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barbaraboeters-pset2
+//
+//  Created by Barbara Boeters on 09-11-16.
+//  Copyright © 2016 Barbara Boeters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
